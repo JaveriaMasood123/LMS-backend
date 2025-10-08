b7fb85a23e6ec8499a5056d6a79470d8675a68fe
JaveriaMasood123 <javeriamails@gmail.com>
2025-10-08 18:15:20 +0500

    Update frontend/backend code

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 // ✅ Allowed frontend origins
 const allowedOrigins = [
   'http://localhost:5173',
-  // 'https://lms-frontend-siql.vercel.app',
+  'https://lms-frontend-ofx4.vercel.app',
 ];
 
 // ✅ CORS configuration
